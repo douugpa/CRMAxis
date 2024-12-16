fc3bdaee5dba654c91ad555800462d133636d6e9
Douglas <douglas@gmail.com>
2024-12-16 09:43:07 -0300

    edit configs firebase

--- src/app/Config/firebase.js
+++ src/app/Config/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeQNFvh0vLDjQL5YkAMHssthF7JfpIwcQ",
-  authDomain: "crmaxis-5b844.firebaseapp.com",
-  projectId: "crmaxis-5b844",
-  storageBucket: "crmaxis-5b844.firebasestorage.app",
-  messagingSenderId: "698136737012",
-  appId: "1:698136737012:web:a4b4c24d572960d40f1aa1"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
